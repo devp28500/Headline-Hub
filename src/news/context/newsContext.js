56c90cf76a8fc64da9d3fc2ae90bb8079faa6273
dev.patel <dev.patel@digihealth.in>
2023-09-14 15:58:04 +0530

    removed loading states

--- src/news/context/newsContext.js
+++ src/news/context/newsContext.js
@@ -1,23 +1,9 @@
-import { createContext, useCallback, useMemo, useState } from "react";
+import { createContext } from "react";
 
 export const NewsContext = createContext(null);
 
 export const NewsContextProvider = ({ children }) => {
-  const [showLoading, setShowLoading] = useState(false);
-
-  const loading = useMemo(() => {
-    return showLoading;
-  }, [showLoading]);
-
-  const updateLoading = useCallback((value) => {
-    setShowLoading(value);
-  }, []);
-
-  return (
-    <NewsContext.Provider value={{ loading, updateLoading }}>
-      {children}
-    </NewsContext.Provider>
-  );
+  return <NewsContext.Provider value={{}}>{children}</NewsContext.Provider>;
 };
 
 export default NewsContext;
