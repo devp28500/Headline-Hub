721e83d9a70429acb43d136d652cf54e188235e8
dev.patel <dev.patel@digihealth.in>
2023-09-14 21:54:06 +0530

    changed import structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AppContextProvider } from "./AppContext";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
